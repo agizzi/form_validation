d5c6dbd10d9fc5164bd450d6fb2baf56bf1ebb11
agizzi <alexandra.n.gizzi@gmail.com>
2019-09-21 19:27:29 -0400

    function for number 4 and problem 5

--- script.js
+++ script.js
@@ -33,18 +33,51 @@ function clearErrorMsgs(field) {
 	}
 }
 
-// function validCarYear(field) {
-// 	const fieldContainer = field.parentNode;
-// 	clearErrorMsgs(field);
-// 	if ((field = '')) {
-// 		return;
-// 	} else if (isNaN(field)) {
-// 		markInvalid(fieldContainer, 'Oops! The year must be a number.');
-// 		// } else {
-// 		// 	markValid(field);
-// 		// }
-// 	}
-// }
+function isDateTodayorLater(date) {
+	let now = new Date(Date.now());
+	let userDate = new Date(date);
+	// if (userDate > now) {
+	// }
+	console.log(now);
+	return userDate <= now;
+}
+
+function validateCardNumber(number) {
+	var regex = new RegExp('^[0-9]{16}$');
+	if (!regex.test(number)) return false;
+
+	return luhnCheck(number);
+}
+
+function luhnCheck(val) {
+	var sum = 0;
+	for (var i = 0; i < val.length; i++) {
+		var intVal = parseInt(val.substr(i, 1));
+		if (i % 2 == 0) {
+			intVal *= 2;
+			if (intVal > 9) {
+				intVal = 1 + intVal % 10;
+			}
+		}
+		sum += intVal;
+	}
+	return sum % 10 == 0;
+}
+
+function sumOfDays(field) {
+	let now = new Date(Date.now());
+	let amount = Number(field.value);
+	let sum = 0;
+	for (let i = 1; i <= amount; i++) {
+		if (now.getDay() === 6 || now.getDay() === 0) {
+			sum += 7;
+		} else {
+			sum += 5;
+		}
+		now.setDate(now.getDate() + 1);
+	}
+	return sum;
+}
 
 query('#parking-form').addEventListener('submit', function(event) {
 	event.preventDefault();
@@ -83,9 +116,12 @@ query('#parking-form').addEventListener('submit', function(event) {
 	// console.log(dateText);
 	if (!dateText) {
 		markInvalid(dateTextField, 'Oops! You need to add a date.');
+	} else if (isDateTodayorLater(dateText)) {
+		markInvalid(dateTextField, 'Oops! Your date needs to be in the future.');
 	} else {
 		markValid(dateTextField);
 	}
+	console.log(dateText);
 });
 
 query('#parking-form').addEventListener('submit', function(event) {
@@ -95,6 +131,8 @@ query('#parking-form').addEventListener('submit', function(event) {
 	// console.log(daysText);
 	if (!daysText) {
 		markInvalid(daysTextField, 'Oops! We need to know how many days you need a pass.');
+	} else if (isNaN(daysText) || daysText < 1 || daysText > 30) {
+		markInvalid(daysTextField, 'Oops! The number of days must be a number between 1 and 30.');
 	} else {
 		markValid(daysTextField);
 	}
@@ -105,7 +143,7 @@ query('#parking-form').addEventListener('submit', function(event) {
 	let creditCardField = query('#credit-card');
 	let creditCardText = creditCardField.value.trim();
 	// console.log(creditCardText);
-	if (!creditCardText) {
+	if (!creditCardText || validateCardNumber(creditCardText) === false) {
 		markInvalid(creditCardField, 'Oops! We need a valid credit card number to process your payment.');
 	} else {
 		markValid(creditCardField);
@@ -117,7 +155,7 @@ query('#parking-form').addEventListener('submit', function(event) {
 	let cvvField = query('#cvv');
 	let cvvText = cvvField.value.trim();
 	// console.log(cvvText);
-	if (!cvvText) {
+	if (!cvvText || isNaN(cvvText) || cvvText.length !== 3) {
 		markInvalid(cvvField, 'Oops! We need your three digit code on the back of your credit card.');
 	} else {
 		markValid(cvvField);
@@ -134,4 +172,14 @@ query('#parking-form').addEventListener('submit', function(event) {
 	} else {
 		markValid(expirationField);
 	}
+});
+
+query('#parking-form').addEventListener('submit', function(event) {
+	event.preventDefault();
+	let daysTextField = query('#days');
+	// let daysText = daysTextField.value.trim();
+	let totalField = query('#total');
+	// let totalText = totalField.value;
+	let total = sumOfDays(daysTextField);
+	console.log(total);
 });
